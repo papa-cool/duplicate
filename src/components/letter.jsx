df48e00603c0e247a3feb682c18145baf97660ff
Emmanuel Delmas <emmanuel.delmas@jobteaser.com>
2020-05-04 22:55:01 +0200

    feat(letter): display points

--- src/components/letter.jsx
+++ src/components/letter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './letter.module.css';
+import { FRENCH_POINTS } from './letters_distribution.js';
 
 let cx = classNames.bind(styles);
 
@@ -8,6 +9,9 @@ const Letter = ({letter, selected}) =>
   <div className={cx('letter', 'square', {selected: selected})}>
     <div className={styles.value}>
       <p>{letter}</p>
+    </div>
+    <div className={styles.point}>
+      <p>{FRENCH_POINTS[letter]}</p>
     </div>
   </div>
 export default Letter;

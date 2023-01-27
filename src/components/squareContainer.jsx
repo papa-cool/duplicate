d8d61936c726cbbcb60fe92a4f7b61da4779c9f4
Emmanuel Delmas <emmanuel.delmas@jobteaser.com>
2023-01-27 22:55:11 +0100

    fixup square refacto

--- src/components/squareContainer.jsx
+++ src/components/squareContainer.jsx
@@ -1,37 +1,37 @@
-import React from 'react';
-import styles from './squareContainer.module.css';
+import React from 'react'
+import styles from './squareContainer.module.css'
 import BasicSquare, {
   DoubleLetterSquare,
   TripleLetterSquare,
   DoubleWordSquare,
   TripleWordSquare
 } from './square.jsx';
-import Letter from './letter.jsx';
+import Letter from './letter.jsx'
 
 
 function SquareContainer(props) {
   let square
   switch(props.type) {
     case 'DoubleLetter':
-      square = <DoubleLetterSquare key={"square"} selected={props.selected} />;
+      square = <DoubleLetterSquare key={"square"} selected={props.selected} />
       break;
     case 'TripleLetter':
-      square = <TripleLetterSquare key={"square"} selected={props.selected} />;
+      square = <TripleLetterSquare key={"square"} selected={props.selected} />
       break;
     case 'DoubleWord':
-      square = <DoubleWordSquare key={"square"} selected={props.selected} />;
+      square = <DoubleWordSquare key={"square"} selected={props.selected} />
       break;
     case 'TripleWord':
-      square = <TripleWordSquare key={"square"} selected={props.selected} />;
+      square = <TripleWordSquare key={"square"} selected={props.selected} />
       break;
     default:
-      square = <BasicSquare key={"square"} selected={props.selected} />;
+      square = <BasicSquare key={"square"} selected={props.selected} />
   }
   return (
     <div className={styles.squareContainer}>
       { props.letter ? [square, <Letter key={"letter"} letter={props.letter} />] : square }
     </div>
-  );
+  )
 }
 
-export default SquareContainer;
+export default SquareContainer

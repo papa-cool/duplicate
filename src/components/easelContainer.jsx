387f025795a852e7c4bb8c729a18ab2cd6e0dede
Emmanuel Delmas <emmanuel.delmas@jobteaser.com>
2023-01-27 23:02:03 +0100

    refacto(easel): migrate from class to functional component

--- src/components/easelContainer.jsx
+++ src/components/easelContainer.jsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import styles from './easelContainer.module.css';
-import Letter from './letter.jsx';
-import PropTypes from 'prop-types';
+import React from 'react'
+import styles from './easelContainer.module.css'
+import Letter from './letter.jsx'
+import PropTypes from 'prop-types'
 
-class EaselContainer extends React.Component {
-  render() {
-    return (
-      <div className={styles.easel}>
-        {
-          this.props.letters.map((letter, index) => {
-            return (
-              <div key={index.toString()} className={styles.square}>
-                { letter ? <Letter letter={letter} selected={false} /> : null }
-              </div>
-            )
-          })
-        }
-      </div>
-    );
-  }
+function EaselContainer(props) {
+  return (
+    <div className={styles.easel}>
+      {
+        props.letters.map((letter, index) => {
+          return (
+            <div key={index.toString()} className={styles.square}>
+              { letter ? <Letter letter={letter} selected={false} /> : null }
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 EaselContainer.propTypes = {
   letters: PropTypes.array
-};
+}
 
-export default EaselContainer;
+export default EaselContainer

c97b2dad6b47f592e78b02272cd141b10794ec98
Emmanuel Delmas <emmanuel.delmas@jobteaser.com>
2023-01-27 23:02:12 +0100

    refacto(player): migrate from class to functional component

--- src/components/playerContainer.jsx
+++ src/components/playerContainer.jsx
@@ -1,86 +1,84 @@
-import React from 'react';
-import styles from './playerContainer.module.css';
+import React from 'react'
+import styles from './playerContainer.module.css'
 
-class PlayerContainer extends React.Component {
+function PlayerContainer(props) {
 
-  tableFromPlayers() {
-    if (Object.entries(this.props.players).length === 0) { return [] }
+  const tableFromPlayers = () => {
+    if (Object.entries(props.players).length === 0) { return [] }
 
     // Extract scores of each player.
     // Fill the score with empty value for display purpose (a better solution may exist on CSS side).
     // We should ensure that last scores are always visible. TODO
     // Add a sum of the scores at the end.
-    let scores = Object.values(this.props.players)
+    let scores = Object.values(props.players)
 
     // Reorganise data from column to line
     return scores[0].map((_, column) => scores.map(row => row[column]))
   }
 
-  totalFromPlayers() {
-    if (Object.entries(this.props.players).length === 0) { return [] }
+  const totalFromPlayers = () => {
+    if (Object.entries(props.players).length === 0) { return [] }
 
-    return Object.values(this.props.players).map((playerScores) => {
+    return Object.values(props.players).map((playerScores) => {
       return playerScores.reduce((sum, currentValue) => sum + currentValue, 0)
     })
   }
 
-  render() {
-    return (
-      <table className={styles.container}>
-        <thead>
-          <tr key="header">
-            {
-              Object.keys(this.props.players).map((name, column) => {
-                return (
-                  <th key={'header-' + column} className={styles.cell}>
-                    <div className={styles.text}>
-                      {name}
-                    </div>
-                  </th>
-                )
-              })
-            }
-          </tr>
-        </thead>
-        <tbody>
+  return (
+    <table className={styles.container}>
+      <thead>
+        <tr key="header">
           {
-            this.tableFromPlayers().map((line, index) => {
+            Object.keys(props.players).map((name, column) => {
               return (
-                <tr key={index}>
-                  {
-                    line.map((cell, column) => {
-                      return (
-                        <td key={index + '-' + column} className={styles.cell}>
-                          <div className={styles.text}>
-                            {cell}
-                          </div>
-                        </td>
-                      )
-                    })
-                  }
-                </tr>
+                <th key={'header-' + column} className={styles.cell}>
+                  <div className={styles.text}>
+                    {name}
+                  </div>
+                </th>
               )
             })
-          } 
-        </tbody>
-        <tfoot>
-          <tr key="footer">
-            {
-              this.totalFromPlayers().map((cell, column) => {
-                return (
-                  <th key={'footer-' + column} className={styles.cell}>
-                    <div className={styles.text}>
-                      {cell}
-                    </div>
-                  </th>
-                )
-              })
-            }
-          </tr>
-        </tfoot>
-      </table>
-    );
-  }
+          }
+        </tr>
+      </thead>
+      <tbody>
+        {
+          tableFromPlayers().map((line, index) => {
+            return (
+              <tr key={index}>
+                {
+                  line.map((cell, column) => {
+                    return (
+                      <td key={index + '-' + column} className={styles.cell}>
+                        <div className={styles.text}>
+                          {cell}
+                        </div>
+                      </td>
+                    )
+                  })
+                }
+              </tr>
+            )
+          })
+        } 
+      </tbody>
+      <tfoot>
+        <tr key="footer">
+          {
+            totalFromPlayers().map((cell, column) => {
+              return (
+                <th key={'footer-' + column} className={styles.cell}>
+                  <div className={styles.text}>
+                    {cell}
+                  </div>
+                </th>
+              )
+            })
+          }
+        </tr>
+      </tfoot>
+    </table>
+  )
 }
 
-export default PlayerContainer;
+export default PlayerContainer

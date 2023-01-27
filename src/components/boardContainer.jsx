209f56534805962cdf81155aba9f5780fea82ebb
Emmanuel Delmas <emmanuel.delmas@jobteaser.com>
2023-01-27 22:53:30 +0100

    refacto(board): migrate from class to functional component

--- src/components/boardContainer.jsx
+++ src/components/boardContainer.jsx
@@ -4,42 +4,37 @@ import SquareContainer from './squareContainer.jsx';
 import BOARD from '../data/board.js';
 import PropTypes from 'prop-types';
 
-class BoardContainer extends React.Component {
-  constructor(props){
-    super(props)
-    this.board = React.createRef()
-  }
+function BoardContainer(props) {
+  const board = React.createRef()
 
   // Select a square.
-  handleClick = (index) => {
-    this.props.handleClick(index)
-    this.board.current.focus()
+  const handleClick = (index) => {
+    props.handleClick(index)
+    board.current.focus()
   }
 
-  render() {
-    return (
-      <div className={styles.board} ref={this.board} onKeyPress={this.props.handleKeyPress.bind(this)} onKeyDown={this.props.handleKeyDown.bind(this)} onBlur={this.props.handleBlur.bind(this)} tabIndex="-1" style={{outline: 'none'}}>
-        {
-          BOARD.map((squares_line, index_line) => {
-            return (
-              <div className={styles.line} key={index_line.toString()}>
-                {
-                  squares_line.map((square, index_square) => {
-                    let key = index_line.toString()+'-'+index_square.toString()
-                    return (
-                      <div className={styles.column} key={key} onClick={this.handleClick.bind(this, key)}>
-                        <SquareContainer index={key} selected={this.props.selectedIndex === key} letter={this.props.letterPositions[key]} type={square.type}/>
-                      </div>
-                    )
-                  })
-                }
-              </div>
-            )
-          })
-        }
-      </div>
-    );
-  }
+  return (
+    <div className={styles.board} ref={board} onKeyPress={props.handleKeyPress} onKeyDown={props.handleKeyDown} onBlur={props.handleBlur} tabIndex="-1" style={{outline: 'none'}}>
+      {
+        BOARD.map((squares_line, index_line) => {
+          return (
+            <div className={styles.line} key={index_line.toString()}>
+              {
+                squares_line.map((square, index_square) => {
+                  let key = index_line.toString()+'-'+index_square.toString()
+                  return (
+                    <div className={styles.column} key={key} onClick={() => handleClick(key)}>
+                      <SquareContainer index={key} selected={props.selectedIndex === key} letter={props.letterPositions[key]} type={square.type}/>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          )
+        })
+      }
+    </div>
+  );
 }
 
 BoardContainer.propTypes = {
